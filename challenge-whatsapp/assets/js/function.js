5854e750c17a433be4e437bf3f03f22e76845acb
LeonardoCesca <leonardocescaf@gmail.com>
2019-05-20 14:28:42 -0300

    ADD filter

--- challenge-whatsapp/assets/js/function.js
+++ challenge-whatsapp/assets/js/function.js
@@ -20,15 +20,40 @@ let html = ''
 users.forEach(function (user) {
     html += `
         <div class="c-peoples__talk">
+        <div class="c-i">
+        <img src="${user.foto}" alt="" class="c-peoples__img">
+        </div>
+        <div class="c-talk-content">
+        <div class="c-talk">${user.nome}</div>
+        <span class="c-talk__subtitle">${user.ultimaMensagem}</span>
+        </div>
+        </div>    
+        `
+})
+
+document.getElementById('users-list').innerHTML = html
+
+document.getElementById("users-list").innerHTML;
+
+document.getElementById("c-search-label").addEventListener("keyup", function (event) {
+    const value = event.target.value;
+    const aa = (users.filter(user => user.nome.toLowerCase().includes(value)));
+    let html2 = '';
+
+    aa.forEach(function (user) {
+        html2 += `
+            <div class="c-peoples__talk">
             <div class="c-i">
-                <img src="${user.foto}" alt="" class="c-peoples__img">
+            <img src="${user.foto}" alt="" class="c-peoples__img">
             </div>
             <div class="c-talk-content">
             <div class="c-talk">${user.nome}</div>
             <span class="c-talk__subtitle">${user.ultimaMensagem}</span>
             </div>
-        </div>    
-    `
-})
+            </div>    
+            `
+    })
 
-document.getElementById('users-list').innerHTML = html
+    document.getElementById('users-list').innerHTML = html2;
+
+});
