1b81c9e90393ee52e839f0903e9393c37a99f65d
LeonardoCesca <leonardocescaf@gmail.com>
2019-05-31 11:32:52 -0300

    ADD PANCA

--- challenge-old-phone/assets/js/functions.js
+++ challenge-old-phone/assets/js/functions.js
@@ -1,203 +1,226 @@
 var count = 0;
-var concaty = '';
+var concaty = "";
 var caracter;
+var time;
 
 function abc() {
-    var input_value = document.getElementById('data2').value;
-    var result = document.getElementById('display').innerHTML;
-    var i = input_value.split(" ");
-    count++;
+  var input_value = document.getElementById("data2").value;
+  var i = input_value.split(" ");
+  count++;
 
-    if (count === 1) {
-        caracter = i[0];
-    } else if (count === 2) {
-        caracter = i[1].substr(0, 1);
-    } else if (count === 3) {
-        caracter = i[1].substr(1, 1);
-    } else if (count === 4) {
-        caracter = i[1].substr(2, 2);
-        count = 0;
-    }
+  if (count === 1) {
+    caracter = i[0];
+    clearTimeout(time);
+  } else if (count === 2) {
+    caracter = i[1].substr(0, 1);
+    clearTimeout(time);
+  } else if (count === 3) {
+    caracter = i[1].substr(1, 1);
+    clearTimeout(time);
+  } else if (count === 4) {
+    caracter = i[1].substr(2, 2);
+    count = 0;
+    clearTimeout(time);
+  }
+  document.getElementById("display").innerHTML = concaty + caracter;
+  console.log(caracter);
+  time = setTimeout(function() {
     concaty = concaty.concat(caracter);
-    result = concaty;
-    document.getElementById('display').innerHTML = caracter;
+    count = 0;
+  }, 1000);
 }
 
 function def() {
-    var input_value = document.getElementById('data3').value;
-    var result = document.getElementById('display').innerHTML;
-    var i = input_value.split(" ");
-    count++;
+  var input_value = document.getElementById("data3").value;
+  var i = input_value.split(" ");
+  count++;
 
-    if (count === 1) {
-        caracter = i[0].substr(0, 1);
-
-    } else if (count === 2) {
-        caracter = i[0].substr(1, 1);
-
-    } else if (count === 3) {
-        caracter = i[0].substr(2, 2);
-
-    } else if (count === 4) {
-        caracter = i[1];
-        count = 0;
-    }
-
+  if (count === 1) {
+    caracter = i[0].substr(0, 1);
+    clearTimeout(time);
+  } else if (count === 2) {
+    caracter = i[0].substr(1, 1);
+    clearTimeout(time);
+  } else if (count === 3) {
+    caracter = i[0].substr(2, 2);
+    clearTimeout(time);
+  } else if (count === 4) {
+    caracter = i[1];
+    count = 0;
+    clearTimeout(time);
+  }
+  document.getElementById("display").innerHTML = concaty + caracter;
+  console.log(caracter);
+  time = setTimeout(function() {
     concaty = concaty.concat(caracter);
-    result = concaty;
-    document.getElementById('display').innerHTML = caracter;
+    count = 0;
+  }, 1000);
 }
 
 function ghi() {
-    var input_value = document.getElementById('data4').value;
-    var result = document.getElementById('display').innerHTML;
-    var i = input_value.split(" ");
-    count++;
+  var input_value = document.getElementById("data4").value;
+  var i = input_value.split(" ");
+  count++;
 
-    if (count === 1) {
-        caracter = result.innerHTML = i[0].substr(0, 1);
-
-    } else if (count === 2) {
-        caracter = result.innerHTML = i[0].substr(1, 1);
-
-    } else if (count === 3) {
-        caracter = result.innerHTML = i[0].substr(2, 2);
-
-    } else if (count === 4) {
-        caracter = result.innerHTML = i[1];
-        count = 0;
-    }
-
+  if (count === 1) {
+    caracter = i[0];
+    clearTimeout(time);
+  } else if (count === 2) {
+    caracter = i[1].substr(0, 1);
+    clearTimeout(time);
+  } else if (count === 3) {
+    caracter = i[1].substr(1, 1);
+    clearTimeout(time);
+  } else if (count === 4) {
+    caracter = i[1].substr(2, 2);
+    count = 0;
+    clearTimeout(time);
+  }
+  document.getElementById("display").innerHTML = concaty + caracter;
+  console.log(caracter);
+  time = setTimeout(function() {
     concaty = concaty.concat(caracter);
-    result = concaty;
-    document.getElementById('display').innerHTML = caracter;
+    count = 0;
+  }, 1000);
 }
 
 function jkl() {
-    var input_value = document.getElementById('data5').value;
-    var result = document.getElementById('display').innerHTML;
-    var i = input_value.split(" ");
-    count++;
+  var input_value = document.getElementById("data5").value;
+  var i = input_value.split(" ");
+  count++;
 
-    if (count === 1) {
-        caracter = result.innerHTML = i[0].substr(0, 1);
-
-    } else if (count === 2) {
-        caracter = result.innerHTML = i[0].substr(1, 1);
-
-    } else if (count === 3) {
-        caracter = result.innerHTML = i[0].substr(2, 2);
-
-    } else if (count === 4) {
-        caracter = result.innerHTML = i[1];
-        count = 0;
-    }
-
+  if (count === 1) {
+    caracter = i[0];
+    clearTimeout(time);
+  } else if (count === 2) {
+    caracter = i[1].substr(0, 1);
+    clearTimeout(time);
+  } else if (count === 3) {
+    caracter = i[1].substr(1, 1);
+    clearTimeout(time);
+  } else if (count === 4) {
+    caracter = i[1].substr(2, 2);
+    count = 0;
+    clearTimeout(time);
+  }
+  document.getElementById("display").innerHTML = concaty + caracter;
+  console.log(caracter);
+  time = setTimeout(function() {
     concaty = concaty.concat(caracter);
-    result = concaty;
-    document.getElementById('display').innerHTML = caracter;
+    count = 0;
+  }, 1000);
 }
 
 function mno() {
-    var input_value = document.getElementById('data6').value;
-    var result = document.getElementById('display').innerHTML;
-    var i = input_value.split(" ");
-    count++;
+  var input_value = document.getElementById("data6").value;
+  var i = input_value.split(" ");
+  count++;
 
-    if (count === 1) {
-        caracter = result.innerHTML = i[0].substr(0, 1);
-
-    } else if (count === 2) {
-        caracter = result.innerHTML = i[0].substr(1, 1);
-
-    } else if (count === 3) {
-        caracter = result.innerHTML = i[0].substr(2, 2);
-
-    } else if (count === 4) {
-        caracter = result.innerHTML = i[1];
-        count = 0;
-    }
-
+  if (count === 1) {
+    caracter = i[0].substr(0, 1);
+    clearTimeout(time);
+  } else if (count === 2) {
+    caracter = i[0].substr(1, 1);
+    clearTimeout(time);
+  } else if (count === 3) {
+    caracter = i[0].substr(2, 2);
+    clearTimeout(time);
+  } else if (count === 4) {
+    caracter = i[1];
+    count = 0;
+    clearTimeout(time);
+  }
+  document.getElementById("display").innerHTML = concaty + caracter;
+  console.log(caracter);
+  time = setTimeout(function() {
     concaty = concaty.concat(caracter);
-    result = concaty;
-    document.getElementById('display').innerHTML = caracter;
+    count = 0;
+  }, 1000);
 }
 
 function pqrs() {
-    var input_value = document.getElementById('data7').value;
-    var result = document.getElementById('display').innerHTML;
-    var i = input_value.split(" ");
-    count++;
+  var input_value = document.getElementById("data7").value;
+  var i = input_value.split(" ");
+  count++;
 
-    if (count === 1) {
-        caracter = result.innerHTML = i[0].substr(0, 1);
-
-    } else if (count === 2) {
-        caracter = result.innerHTML = i[0].substr(1, 1);
-
-    } else if (count === 3) {
-        caracter = result.innerHTML = i[0].substr(2, 2);
-
-    } else if (count === 4) {
-        caracter = result.innerHTML = i[1];
-        count = 0;
-    }
-
+  if (count === 1) {
+    caracter = i[0];
+    clearTimeout(time);
+  } else if (count === 2) {
+    caracter = i[1].substr(0, 1);
+    clearTimeout(time);
+  } else if (count === 3) {
+    caracter = i[1].substr(1, 1);
+    clearTimeout(time);
+  } else if (count === 4) {
+    caracter = i[1].substr(2, 1);
+    clearTimeout(time);
+  } else if (count === 5) {
+    caracter = i[1].substr(3, 3);
+    count = 0;
+    clearTimeout(time);
+  }
+  document.getElementById("display").innerHTML = concaty + caracter;
+  console.log(caracter);
+  time = setTimeout(function() {
     concaty = concaty.concat(caracter);
-    result = concaty;
-    document.getElementById('display').innerHTML = caracter;
-
+    count = 0;
+  }, 1000);
 }
 
 function tuv() {
-    var input_value = document.getElementById('data8').value;
-    var result = document.getElementById('display').innerHTML;
-    var i = input_value.split(" ");
-    count++;
+  var input_value = document.getElementById("data8").value;
+  var i = input_value.split(" ");
+  count++;
 
-    if (count === 1) {
-        caracter = result.innerHTML = i[0].substr(0, 1);
-
-    } else if (count === 2) {
-        caracter = result.innerHTML = i[0].substr(1, 1);
-
-    } else if (count === 3) {
-        caracter = result.innerHTML = i[0].substr(2, 2);
-
-    } else if (count === 4) {
-        caracter = result.innerHTML = i[1];
-        count = 0;
-    }
-
+  if (count === 1) {
+    caracter = i[0];
+    clearTimeout(time);
+  } else if (count === 2) {
+    caracter = i[1].substr(0, 1);
+    clearTimeout(time);
+  } else if (count === 3) {
+    caracter = i[1].substr(1, 1);
+    clearTimeout(time);
+  } else if (count === 4) {
+    caracter = i[1].substr(2, 2);
+    count = 0;
+    clearTimeout(time);
+  }
+  document.getElementById("display").innerHTML = concaty + caracter;
+  console.log(caracter);
+  time = setTimeout(function() {
     concaty = concaty.concat(caracter);
-    result = concaty;
-    document.getElementById('display').innerHTML = caracter;
+    count = 0;
+  }, 1000);
 }
 
 function wxyz() {
-    var input_value = document.getElementById('data9').value;
-    var result = document.getElementById('display').innerHTML;
-    var i = input_value.split(" ");
-    count++;
+  var input_value = document.getElementById("data9").value;
+  var i = input_value.split(" ");
+  count++;
 
-    if (count === 1) {
-        caracter = result.innerHTML = i[0].substr(0, 1);
-
-    } else if (count === 2) {
-        caracter = result.innerHTML = i[0].substr(1, 1);
-
-    } else if (count === 3) {
-        caracter = result.innerHTML = i[0].substr(2, 2);
-
-    } else if (count === 4) {
-        caracter = result.innerHTML = i[0].substr(3, 3);
-
-    } else if (count === 5) {
-        caracter = result.innerHTML = i[1];
-        count = 0;
-    }
-
+  if (count === 1) {
+    caracter = i[0].substr(0, 1);
+    clearTimeout(time);
+  } else if (count === 2) {
+    caracter = i[0].substr(1, 1);
+    clearTimeout(time);
+  } else if (count === 3) {
+    caracter = i[0].substr(2, 1);
+    clearTimeout(time);
+  } else if (count === 4) {
+    caracter = i[0].substr(3, 3);
+    clearTimeout(time);
+  } else if (count === 5) {
+    caracter = i[1];
+    count = 0;
+    clearTimeout(time);
+  }
+  document.getElementById("display").innerHTML = concaty + caracter;
+  console.log(caracter);
+  time = setTimeout(function() {
     concaty = concaty.concat(caracter);
-    result = concaty;
-    document.getElementById('display').innerHTML = caracter;
+    count = 0;
+  }, 1000);
 }
