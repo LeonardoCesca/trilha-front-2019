702f7de76e5b7c5a6f75677b270292a4aa051124
LeonardoCesca <leonardocescaf@gmail.com>
2019-06-10 15:42:00 -0300

    ADD more modifications

---
+++ challenge-dashboard/src/components/Aside.vue
@@ -0,0 +1,31 @@
+<template>
+  <aside>
+    <header>
+      <p>Header</p>
+    </header>
+  </aside>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+aside {
+  background-color: #222d30;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 20%;
+}
+
+header {
+  background: #357eab;
+  height: 59.9px;
+}
+
+p {
+  margin: 0;
+  padding: 0;
+}
+</style>
