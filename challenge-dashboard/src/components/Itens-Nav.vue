ff953b82e913688561dd9374d89535a81006659b
LeonardoCesca <leonardocescaf@gmail.com>
2019-06-11 10:13:12 -0300

    ADD modifications into itens-nav

--- challenge-dashboard/src/components/Itens-Nav.vue
+++ challenge-dashboard/src/components/Itens-Nav.vue
@@ -1,33 +1,71 @@
 <template>
- <section class="c-itens">
-     <h4 class="c-itens__title">Header</h4>
-     <ul class="c-itens__list">
-         <a href="" class="c-itens__link">
-         <li class="c-itens__item">
-            <img class="c-itens__img" src="../assets/img/edit-document.png" alt="profile image" title="profile">
-            <span class="c-itens__name">Home</span> 
-         </li>
-         </a>
-         <a href="" class="c-itens__link">
-         <li class="c-itens__item">
-            <img class="c-itens__img" src="../assets/img/edit-document.png" alt="profile image" title="profile">
-            <span class="c-itens__name">Doctors</span> 
-         </li>
-         </a>
-         <a href="" class="c-itens__link">
-         <li class="c-itens__item">
-            <img class="c-itens__img" src="../assets/img/edit-document.png" alt="profile image" title="profile">
-            <span class="c-itens__name">Nurses</span> 
-         </li>
-         </a>
-         <a href="" class="c-itens__link">
-         <li class="c-itens__item">
-            <img class="c-itens__img" src="../assets/img/edit-document.png" alt="profile image" title="profile">
-            <span class="c-itens__name">Patients</span> 
-         </li>
-         </a>
-     </ul>
- </section>
+  <section class="c-itens">
+    <div class="c-itens__content">
+      <h4 class="c-itens__title">Header</h4>
+    </div>
+    <ul class="c-itens__list">
+      <a href class="c-itens__link">
+        <li class="c-itens__item">
+          <svg
+            class="c-itens__img"
+            xmlns="http://www.w3.org/2000/svg"
+            data-name="Layer 1"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M21,12.5a5.52,5.52,0,0,0-3.51-5.12,5.49,5.49,0,0,0-11,0A5.5,5.5,0,0,0,8.5,18l.2,0a6.91,6.91,0,0,1-1.24,2.39A1,1,0,0,0,8.24,22h7.52a1,1,0,0,0,.78-1.63A6.91,6.91,0,0,1,15.3,18l.2,0A5.51,5.51,0,0,0,21,12.5ZM10.06,20a8.89,8.89,0,0,0,.81-2.56,5.47,5.47,0,0,0,1.13-.7,5.47,5.47,0,0,0,1.13.7A8.89,8.89,0,0,0,13.94,20Zm2.72-5.3-.08-.08-.08-.08-.07,0-.18-.09-.18-.06h-.38l-.18.06a.56.56,0,0,0-.17.09l-.08,0-.08.08-.08.08A3.5,3.5,0,1,1,7.75,9.08,1.11,1.11,0,0,0,7.91,9a1.42,1.42,0,0,0,.18-.08.83.83,0,0,0,.14-.13.62.62,0,0,0,.21-.31.61.61,0,0,0,.07-.17.69.69,0,0,0,0-.2,1,1,0,0,0,0-.17,3.4,3.4,0,0,1,0-.45,3.5,3.5,0,0,1,7,0,3.4,3.4,0,0,1,0,.45.81.81,0,0,0,0,.16.74.74,0,0,0,0,.21.61.61,0,0,0,.07.17.62.62,0,0,0,.21.31.83.83,0,0,0,.14.13,1.42,1.42,0,0,0,.18.08,1.11,1.11,0,0,0,.16.07,3.5,3.5,0,1,1-3.47,5.62Z"
+            ></path>
+          </svg>
+          <span class="c-itens__name">Home</span>
+        </li>
+      </a>
+      <a href class="c-itens__link">
+        <li class="c-itens__item">
+          <svg
+            class="c-itens__img"
+            xmlns="http://www.w3.org/2000/svg"
+            data-name="Layer 1"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M21,12.5a5.52,5.52,0,0,0-3.51-5.12,5.49,5.49,0,0,0-11,0A5.5,5.5,0,0,0,8.5,18l.2,0a6.91,6.91,0,0,1-1.24,2.39A1,1,0,0,0,8.24,22h7.52a1,1,0,0,0,.78-1.63A6.91,6.91,0,0,1,15.3,18l.2,0A5.51,5.51,0,0,0,21,12.5ZM10.06,20a8.89,8.89,0,0,0,.81-2.56,5.47,5.47,0,0,0,1.13-.7,5.47,5.47,0,0,0,1.13.7A8.89,8.89,0,0,0,13.94,20Zm2.72-5.3-.08-.08-.08-.08-.07,0-.18-.09-.18-.06h-.38l-.18.06a.56.56,0,0,0-.17.09l-.08,0-.08.08-.08.08A3.5,3.5,0,1,1,7.75,9.08,1.11,1.11,0,0,0,7.91,9a1.42,1.42,0,0,0,.18-.08.83.83,0,0,0,.14-.13.62.62,0,0,0,.21-.31.61.61,0,0,0,.07-.17.69.69,0,0,0,0-.2,1,1,0,0,0,0-.17,3.4,3.4,0,0,1,0-.45,3.5,3.5,0,0,1,7,0,3.4,3.4,0,0,1,0,.45.81.81,0,0,0,0,.16.74.74,0,0,0,0,.21.61.61,0,0,0,.07.17.62.62,0,0,0,.21.31.83.83,0,0,0,.14.13,1.42,1.42,0,0,0,.18.08,1.11,1.11,0,0,0,.16.07,3.5,3.5,0,1,1-3.47,5.62Z"
+            ></path>
+          </svg>
+          <span class="c-itens__name">Doctors</span>
+        </li>
+      </a>
+      <a href class="c-itens__link">
+        <li class="c-itens__item">
+          <svg
+            class="c-itens__img"
+            xmlns="http://www.w3.org/2000/svg"
+            data-name="Layer 1"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M21,12.5a5.52,5.52,0,0,0-3.51-5.12,5.49,5.49,0,0,0-11,0A5.5,5.5,0,0,0,8.5,18l.2,0a6.91,6.91,0,0,1-1.24,2.39A1,1,0,0,0,8.24,22h7.52a1,1,0,0,0,.78-1.63A6.91,6.91,0,0,1,15.3,18l.2,0A5.51,5.51,0,0,0,21,12.5ZM10.06,20a8.89,8.89,0,0,0,.81-2.56,5.47,5.47,0,0,0,1.13-.7,5.47,5.47,0,0,0,1.13.7A8.89,8.89,0,0,0,13.94,20Zm2.72-5.3-.08-.08-.08-.08-.07,0-.18-.09-.18-.06h-.38l-.18.06a.56.56,0,0,0-.17.09l-.08,0-.08.08-.08.08A3.5,3.5,0,1,1,7.75,9.08,1.11,1.11,0,0,0,7.91,9a1.42,1.42,0,0,0,.18-.08.83.83,0,0,0,.14-.13.62.62,0,0,0,.21-.31.61.61,0,0,0,.07-.17.69.69,0,0,0,0-.2,1,1,0,0,0,0-.17,3.4,3.4,0,0,1,0-.45,3.5,3.5,0,0,1,7,0,3.4,3.4,0,0,1,0,.45.81.81,0,0,0,0,.16.74.74,0,0,0,0,.21.61.61,0,0,0,.07.17.62.62,0,0,0,.21.31.83.83,0,0,0,.14.13,1.42,1.42,0,0,0,.18.08,1.11,1.11,0,0,0,.16.07,3.5,3.5,0,1,1-3.47,5.62Z"
+            ></path>
+          </svg>
+          <span class="c-itens__name">Nurses</span>
+        </li>
+      </a>
+      <a href class="c-itens__link">
+        <li class="c-itens__item">
+          <svg
+            class="c-itens__img"
+            xmlns="http://www.w3.org/2000/svg"
+            data-name="Layer 1"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M21,12.5a5.52,5.52,0,0,0-3.51-5.12,5.49,5.49,0,0,0-11,0A5.5,5.5,0,0,0,8.5,18l.2,0a6.91,6.91,0,0,1-1.24,2.39A1,1,0,0,0,8.24,22h7.52a1,1,0,0,0,.78-1.63A6.91,6.91,0,0,1,15.3,18l.2,0A5.51,5.51,0,0,0,21,12.5ZM10.06,20a8.89,8.89,0,0,0,.81-2.56,5.47,5.47,0,0,0,1.13-.7,5.47,5.47,0,0,0,1.13.7A8.89,8.89,0,0,0,13.94,20Zm2.72-5.3-.08-.08-.08-.08-.07,0-.18-.09-.18-.06h-.38l-.18.06a.56.56,0,0,0-.17.09l-.08,0-.08.08-.08.08A3.5,3.5,0,1,1,7.75,9.08,1.11,1.11,0,0,0,7.91,9a1.42,1.42,0,0,0,.18-.08.83.83,0,0,0,.14-.13.62.62,0,0,0,.21-.31.61.61,0,0,0,.07-.17.69.69,0,0,0,0-.2,1,1,0,0,0,0-.17,3.4,3.4,0,0,1,0-.45,3.5,3.5,0,0,1,7,0,3.4,3.4,0,0,1,0,.45.81.81,0,0,0,0,.16.74.74,0,0,0,0,.21.61.61,0,0,0,.07.17.62.62,0,0,0,.21.31.83.83,0,0,0,.14.13,1.42,1.42,0,0,0,.18.08,1.11,1.11,0,0,0,.16.07,3.5,3.5,0,1,1-3.47,5.62Z"
+            ></path>
+          </svg>
+          <span class="c-itens__name">Patients</span>
+        </li>
+      </a>
+    </ul>
+  </section>
 </template>
 
 <script>
@@ -39,7 +77,22 @@ export default {};
   display: flex;
   flex-direction: column;
   float: left;
-  padding-left: 10px;
+
+  &__title {
+    color: #303d40;
+    padding-left: 15px;
+    float: left;
+  }
+
+  &__content {
+    display: flex;
+    align-items: center;
+    width: 20vw;
+    height: 4vh;
+    padding: 0;
+    margin: 10px 0;
+    background-color: #1a2527;
+  }
 
   &__list {
     text-align: initial;
@@ -51,11 +104,26 @@ export default {};
   &__img {
     width: 20px;
     height: 20px;
+    fill: #a6adb2;
+    padding-left: 15px;
+  }
+
+  &__item {
+    display: flex;
+    align-items: center;
+    width: 20vw;
+    height: 4vh;
+    margin-bottom: 2px;
   }
 
   &__link {
-      text-decoration: none;
-      color: #fcfcfc;
+    text-decoration: none;
+    color: #fcfcfc;
+  }
+
+  &__name {
+    padding-left: 5px;
+    color: #a6adb2;
   }
 }
 </style>
