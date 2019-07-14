def65a4e4d7ae274f422a4c71c4c34c631c0f98a
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-14 20:11:37 -0300

    ADD input forms and forms component

---
+++ Angular/poc-forms/src/app/forms/forms.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.scss']
+})
+export class FormsComponent implements OnInit {
+  name = new FormControl('');
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
