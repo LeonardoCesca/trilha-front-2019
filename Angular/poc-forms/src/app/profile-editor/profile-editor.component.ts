1cff12eb9ff8574317a302beecea66c589794ad6
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-15 08:49:41 -0300

    ADD new group - Address

--- Angular/poc-forms/src/app/profile-editor/profile-editor.component.ts
+++ Angular/poc-forms/src/app/profile-editor/profile-editor.component.ts
@@ -9,7 +9,13 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class ProfileEditorComponent implements OnInit {
   profileForm = new FormGroup({
     firstName: new FormControl(''),
-    lastName: new FormControl('')
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
   });
 
   constructor() { }
