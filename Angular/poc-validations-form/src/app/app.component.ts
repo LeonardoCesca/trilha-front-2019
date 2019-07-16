59d1afbbda8b498e078b93076d79a8e284eb0b87
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-16 20:35:22 -0300

    ADD form into app.component.ts

--- Angular/poc-validations-form/src/app/app.component.ts
+++ Angular/poc-validations-form/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+// import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'poc-validations-form';
+  formularioDeUsuario: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.criarFormularioDeUsuario();
+  }
+
+  criarFormularioDeUsuario() {
+    this.formularioDeUsuario = this.fb.group({
+      nome: [''],
+      email: [''],
+      cpf: [''],
+      nascimento: [''],
+      senha: [''],
+      confirmarSenha: [''],
+    });
+  }
+
+  enviarDados() {
+    console.log(this.formularioDeUsuario.value);
+  }
 }
