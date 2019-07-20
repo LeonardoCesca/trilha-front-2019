fe6cb7cd8b0a4809548d8350985957c427c3976c
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-20 19:20:42 -0300

    ADD HttpClientModule into app module

--- Angular/poc-table-json-server/src/app/app.module.ts
+++ Angular/poc-table-json-server/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
@@ -11,6 +12,7 @@ import { TableComponent } from './table/table.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
