67046bfb5431845957067ea12ae728a12e093524
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-20 19:49:58 -0300

    FIX employees.ts

--- Angular/poc-table-json-server/src/employees.ts
+++ Angular/poc-table-json-server/src/employees.ts
@@ -4,5 +4,10 @@ export class Employees {
     lastName: string;
     email: string;
 
-    constructor(){}
+    constructor(id, firstName, lastName, email){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
