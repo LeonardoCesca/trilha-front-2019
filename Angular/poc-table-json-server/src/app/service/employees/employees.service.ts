b6439f26c3a3ad7dbb12b074fc3da6b4442830a6
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-19 22:55:13 -0300

    ADD new service - employees

--- Angular/poc-table-json-server/src/app/service/employees/employees.service.ts
+++ Angular/poc-table-json-server/src/app/service/employees/employees.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
+
+  getRegisters(): Observable<any> {
+    const url = '/employees';
+    return this.http.get(url);
+  }
 }
