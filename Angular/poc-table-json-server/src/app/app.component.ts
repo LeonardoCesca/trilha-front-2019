74f925b9216c15d6a7299451e204fccb1c62e23d
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-20 22:08:08 -0300

    REMOVE responsabilities that app.component

--- Angular/poc-table-json-server/src/app/app.component.ts
+++ Angular/poc-table-json-server/src/app/app.component.ts
@@ -1,31 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { EmployeesService } from './service/employees/employees.service';
-import { Employees } from 'src/employees';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'poc-table-json-server';
+export class AppComponent {
 
-  constructor(private es : EmployeesService) {}
-
-  columns = ['Id', 'First Name', 'Last Name', 'Email'];
-
-  index = ['id', 'firstName', 'lastName', 'email'];
-
-  employees : Employees[] = [];
-
-  ngOnInit(): void {
-    this.es.getRegisters().subscribe(
-      (response) => {
-        this.employees = response;
-      },
-      (error) => {
-        console.log ("Erro");
-      }
-    )
-  }
 }
