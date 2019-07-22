3e7bfb4e30429f87d7042cc640bdf2074ade4099
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-22 17:05:47 -0300

    UPDATE app.component

--- Angular/poc-dynamic-checkbox/src/app/app.component.ts
+++ Angular/poc-dynamic-checkbox/src/app/app.component.ts
@@ -1,30 +1,10 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
-  musicForm: FormGroup;
-  musicPreferences = [
-    { id: 1, genre: 'Pop' },
-    { id: 2, genre: 'Rock' },
-    { id: 3, genre: 'Techno' },
-    { id: 4, genre: 'Hiphop' }
-  ];
-
-  constructor(private fb: FormBuilder) {
-    const formControls = this.musicPreferences.map(control => new FormControl(false));
-
-    this.musicForm = this.fb.group({
-      musicPreferences: new FormArray(formControls)
-    });
-  }
-
-  submit() {
-
-  }
+  title = 'poc-dynamic-checkbox';
 }
