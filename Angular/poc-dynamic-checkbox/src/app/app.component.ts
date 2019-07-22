aaba3de33892f4b2f781f8f719e50ccb0baebab2
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-22 16:05:22 -0300

    ADD FormGroup

--- Angular/poc-dynamic-checkbox/src/app/app.component.ts
+++ Angular/poc-dynamic-checkbox/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'poc-dynamic-checkbox';
+  musicForm: FormGroup;
+  musicPreferences = [
+    { id: 1, genre: 'Pop' },
+    { id: 2, genre: 'Rock' },
+    { id: 3, genre: 'Techno' },
+    { id: 4, genre: 'Hiphop' }
+  ];
 }
