a01e7fc9107c9985af94246a38d6ac2ef85ef692
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-24 14:14:53 -0300

    ADD infoFilterPipe

--- Angular/poc-searchbar/src/app/app.module.ts
+++ Angular/poc-searchbar/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InfoFilterPipe } from './info-filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoFilterPipe
   ],
   imports: [
     BrowserModule
