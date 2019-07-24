db008298761bc946db1cf39b36ffdc3438f3be43
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-24 14:16:48 -0300

    ADD FormsModule

--- Angular/poc-searchbar/src/app/app.module.ts
+++ Angular/poc-searchbar/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InfoFilterPipe } from './info-filter.pipe';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { InfoFilterPipe } from './info-filter.pipe';
     InfoFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
