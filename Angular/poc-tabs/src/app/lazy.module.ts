e209eb4f7649fda3f524ed7b355dbfb7bd1c2ed3
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-27 16:07:42 -0300

    import Routes into lazy.module

--- Angular/poc-tabs/src/app/lazy.module.ts
+++ Angular/poc-tabs/src/app/lazy.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LazyParentComponent } from './lazy-parent/lazy-parent.component';
 import { LazyChildComponent } from './lazy-child/lazy-child.component';
-
-
+import { Routes, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LazyParentComponent, LazyChildComponent],
